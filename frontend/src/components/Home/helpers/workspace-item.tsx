4cd59ff5d976c224247df089bcbd903f5a7c7999
ayoub bougue <63568455+ayoubbouguettaya@users.noreply.github.com>
2024-02-18 19:27:52 +0100

    ep5-part1

---
+++ frontend/src/components/Home/helpers/workspace-item.tsx
@@ -0,0 +1,115 @@
+import React,{useState} from 'react'
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion";
+  import { CopyMinus, File, FilePlus, FolderPlus, Layers } from "lucide-react";
+  
+export type WorkSpaceItemProps = {
+    workspaceName: string;
+    items: WorkSpaceItems[];
+  };
+  
+export type WorkSpaceItems = {
+    name: string;
+    type: "folder" | "file";
+    items?: WorkSpaceItems[];
+  };
+  
+  const WorkspaceItem = ({ data }: { data: WorkSpaceItemProps }) => {
+    const [activeItemPath, setactiveItemPath] = useState("");
+  
+
+    const addFolder = () => {
+/* POST REQUEST  
+   body name of Folder / file and related Path
+   response => to update the state .  
+*/     
+
+    }
+
+    const addFile = () => {
+        
+        /* POST REQUEST  
+           body name of Folder / file and related Path
+           response => to update the state .  
+        */     
+            }
+
+    return (
+      <div className="text-gray-600">
+        <div className=" flex content-start justify-between mb-3">
+          <span className="mr-auto font-medium"> {data.workspaceName}</span>
+          <FolderPlus size={20} className="mr-1" />
+          <FilePlus size={20} className="mr-1" />
+          <CopyMinus size={20} />
+        </div>
+        <p className="text-pink-500">{activeItemPath}</p>
+        <Accordion type="multiple">
+          {data.items.map((workspaceItem) => (
+            <WorkspaceItemFolder
+              activeItemPath={activeItemPath}
+              basePath=""
+              key={workspaceItem.name}
+              setactiveItemPath={setactiveItemPath}
+              data={workspaceItem}
+            />
+          ))}
+        </Accordion>
+      </div>
+    );
+  };
+  
+  const WorkspaceItemFolder = ({
+    data,
+    setactiveItemPath,
+    activeItemPath,
+    basePath,
+  }: {
+    data: WorkSpaceItems;
+    activeItemPath: string;
+    setactiveItemPath: React.Dispatch<React.SetStateAction<string>>;
+    basePath: string;
+  }) => {
+    const relatedPath = `${basePath}/${data.name}`;
+  
+    if (data.type === "file")
+      return (
+        <div
+          onClick={() => setactiveItemPath(relatedPath)}
+          className={` ${activeItemPath === relatedPath && "bg-slate-300"}
+          text-sm flex min-w-32  overflow-hidden py-1 rounded`}
+        >
+          <File className={`ml-4 mr-1`} width={20} />
+          {data.name}
+        </div>
+      );
+  
+    return (
+      <AccordionItem value={data.name}>
+        <AccordionTrigger>
+          <button className={`${activeItemPath === relatedPath && "bg-slate-300"} rounded`} onClick={() => setactiveItemPath(relatedPath)}>
+            {" "}
+            {data.name}
+          </button>{" "}
+        </AccordionTrigger>
+        <AccordionContent>
+          {data.items?.map((item) => (
+            <WorkspaceItemFolder
+              activeItemPath={activeItemPath}
+              basePath={relatedPath}
+              setactiveItemPath={setactiveItemPath}
+              key={item.name}
+              data={item}
+            />
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    );
+  };
+  
+
+export default WorkspaceItem
--- frontend/src/components/Home/helpers/workspace-panel.tsx
+++ frontend/src/components/Home/helpers/workspace-panel.tsx
@@ -1,12 +1,8 @@
+"use client";
 import React from "react";
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { CopyMinus, File, FilePlus, FolderPlus, Layers } from "lucide-react";
+import { Layers } from "lucide-react";
+import WorkspaceItem, { WorkSpaceItemProps } from "./workspace-item";
 
 type Props = {};
 
@@ -37,12 +33,12 @@ const workspacesData: WorkSpaceItemProps[] = [
       {
         type: "folder",
         name: "folder-2",
-        items:  [
-              {
-                type: "file",
-                name: "folder-98.md",
-              },
-            ],
+        items: [
+          {
+            type: "file",
+            name: "folder-98.md",
+          },
+        ],
       },
       {
         type: "file",
@@ -53,6 +49,9 @@ const workspacesData: WorkSpaceItemProps[] = [
 ];
 
 const WorkspacePanelComponent = (props: Props) => {
+
+  // Http calls to get the workspaces
+
   return (
     <div className="font-mono w-96 bg-slate-100 p-3 border border-r-slate-200 h-dvh">
       <h2 className="bg-slate-500 text-green-300 p-2 rounded flex font-semibold border-b pb-2 mb-2">
@@ -68,53 +67,5 @@ const WorkspacePanelComponent = (props: Props) => {
   );
 };
 
-type WorkSpaceItemProps = {
-  workspaceName: string;
-  items: WorkSpaceItems[];
-};
-
-type WorkSpaceItems = {
-  name: string;
-  type: "folder" | "file";
-  items?: WorkSpaceItems[];
-};
-
-const WorkspaceItem = ({ data }: { data: WorkSpaceItemProps }) => {
-  return (
-    <div className="text-gray-600">
-      <div className=" flex content-start justify-between mb-3">
-        <span className="mr-auto font-medium"> {data.workspaceName}</span>
-        <FolderPlus size={20} className="mr-1" />
-        <FilePlus size={20} className="mr-1"/>
-        <CopyMinus size={20} />
-      </div>
-      <Accordion type="multiple">
-        {data.items.map((workspaceItem) => (
-          <WorkspaceItemFolder key={workspaceItem.name} data={workspaceItem} />
-        ))}
-      </Accordion>
-    </div>
-  );
-};
-
-const WorkspaceItemFolder = ({ data }: { data: WorkSpaceItems }) => {
-  if (data.type === "file")
-    return (
-      <div className="text-sm flex min-w-32  overflow-hidden py-1">
-        <File className="ml-4 mr-1" width={20} /> {data.name}
-      </div>
-    );
-
-  return (
-    <AccordionItem value={data.name}>
-      <AccordionTrigger>{data.name}</AccordionTrigger>
-      <AccordionContent>
-        {data.items?.map((item) => (
-          <WorkspaceItemFolder key={item.name} data={item} />
-        ))}
-      </AccordionContent>
-    </AccordionItem>
-  );
-};
 
 export default WorkspacePanelComponent;
